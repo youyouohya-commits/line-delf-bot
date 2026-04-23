ca8aa8d2f120f8533461694944422c64cadbf879
youyouohya-commits <youyouohya@gmail.com>
2026-04-24 01:16:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import date
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 app = Flask(__name__)
